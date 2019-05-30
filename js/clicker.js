c8ba8f87f59e9e6571ba9df08842520a9d417e39
Samson <47623975+samsonloftin@users.noreply.github.com>
2019-05-30 01:41:25 -0700

    feat: Cat List
    The cat clicker will only display one cat at a time. This improves visibility of each picture and ease of use without the need of constant scrolling

--- js/clicker.js
+++ js/clicker.js
@@ -1,32 +1,75 @@
-let catBox = document.querySelector(".cat-box");
+let catBox = document.querySelector(".cat-box"),
+    catListDisplay = document.createElement("div"),
+    catListUl = document.createElement("ul"),
+    imageCounter = 0,
+    catArray = [],
+    catArrayImg = [];
 
-function cat(name, image) {
-    let catDiv = document.createElement("div"),
-        catName = document.createElement("h3"),
-        catImg = document.createElement("img"),
-        catCounter = document.createElement("p"),
-        counter = 0;
-
-    catImg.src = image;
-
-    catDiv.appendChild(catName);
-    catDiv.appendChild(catImg);
-    catDiv.appendChild(catCounter);
-    catBox.appendChild(catDiv);
-
-    catName.setAttribute("class", "cat-title");
-    catName.innerHTML = name;
-    catCounter.setAttribute("class", "counter");
-    catCounter.innerHTML = counter;
-
-    catImg.addEventListener("click", () => {
-        counter += 1;
-        catCounter.innerHTML = counter;
-    }, false);
+function catArrayFunc(name, image) {
+    catArray.push(name);
+    catArrayImg.push(image);
 };
 
-cat("sally", "img/cat.gif");
-cat("markie", "img/cat02.gif");
-cat("jake", "img/cat03.gif");
-cat("mary", "img/cat04.gif");
-cat("moozie", "img/cat05.gif");
+function hideCats() {
+    catArray.forEach(cat => {
+        let findCat = document.getElementById(cat);
+        findCat.setAttribute("class","cat hide");
+    });
+};
+
+function catElements() {
+
+    catArray.forEach(cat => {
+        let catListText = document.createElement("li"),
+            catDiv = document.createElement("div"),
+            catImg = document.createElement("img"),
+            catName = document.createElement("h3"),
+            catCounter = document.createElement("p"),
+            counter = 0;
+
+        imageCounter += 1;
+        catImg.src = "img/cat0" + imageCounter + ".gif";
+
+        catListText.innerHTML = cat;
+        catListText.setAttribute("id", ("l" + cat));
+        catImg.setAttribute("class", "kittyImg");
+        catDiv.setAttribute("class", "cat hide");
+        catDiv.setAttribute("id", cat);
+        catName.setAttribute("class", "cat-title");
+        catCounter.setAttribute("class", "counter");
+        catCounter.innerHTML = counter;
+        catName.innerHTML = cat;
+
+        // Creating the cat list
+        catBox.appendChild(catListDisplay);
+        catListDisplay.appendChild(catListUl);
+        catListUl.appendChild(catListText);
+        catListText.appendChild(catDiv);
+        catDiv.appendChild(catName);
+        catDiv.appendChild(catImg);
+        catDiv.appendChild(catCounter);
+
+        catImg.addEventListener("click", () => {
+            counter += 1;
+            catCounter.innerHTML = counter;
+        }, false);
+
+        catListText.addEventListener("click", () => {
+            let gettingAttr = catListText.getAttribute("id");
+            
+            hideCats();
+            catArray.forEach(element => {
+                if (gettingAttr === ("l" + element)) {
+                    catDiv.setAttribute("class", "cat");
+                }
+            });
+        });
+    });
+}
+
+catArrayFunc("sally", "img/cat.gif");
+catArrayFunc("markie", "img/cat02.gif");
+catArrayFunc("jake", "img/cat03.gif");
+catArrayFunc("mary", "img/cat04.gif");
+catArrayFunc("moozie", "img/cat05.gif");
+catElements();
